28f92aa721ab27cfa308d45c4a97e19b98d4d88d
en-chang <enchangcs@gmail.com>
2021-06-02 23:33:13 -0500

    Add Error Boundry component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends React.Component {
@@ -35,7 +36,9 @@ class App extends React.Component {
         {!robots.length
           ? <h1>Loading</h1>
           : <Scroll>
-              <CardList robots={filteredRobots} />
+              <ErrorBoundry>
+                <CardList robots={filteredRobots} />
+              </ErrorBoundry>
             </Scroll>
         }
       </div>

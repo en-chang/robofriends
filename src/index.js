8816d33b3b59ca7000547c28e8f1c27f834f9c1d
en-chang <enchangcs@gmail.com>
2021-06-29 16:57:31 -0500

    Attempt to update packages and remove logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ requestRobots, searchRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 ReactDOM.render(
   <React.StrictMode>

1df98cf8647b60252991064968dbf72d1eed0bb2
en-chang <enchangcs@gmail.com>
2021-07-02 17:49:55 -0500

    Test screenshot and coverage

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,5 +2,5 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('expect to render Card component', () => {
-  expect(shallow(<Card />).length).toEqual(1)
+  expect(shallow(<Card />)).toMatchSnapshot();
 })

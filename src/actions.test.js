e7e8c40eb88e3dfbbb60c27967ec2e2eaf609e3c
en-chang <enchangcs@gmail.com>
2021-07-04 20:56:25 -0500

    Wrote basic test for redux actions

---
+++ src/actions.test.js
@@ -0,0 +1,32 @@
+import { 
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+  CHANGE_SEARCH_FIELD 
+} from './constants';
+
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+import * as actions from './actions';
+
+const mockStore = configureMockStore([thunkMiddleware])
+
+it('should create an action to search robots', () => {
+  const text = 'woo'
+  const expectedAction = {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+  }
+  expect(actions.setSearchField(text)).toEqual(expectedAction)
+})
+
+it('handles requesting robots API', () => {
+  const store = mockStore();
+  store.dispatch(actions.requestRobots())
+  const action = store.getActions();
+  const expectedAction = {
+    type: REQUEST_ROBOTS_PENDING,
+  }
+  expect(action[0]).toEqual(expectedAction)
+})
